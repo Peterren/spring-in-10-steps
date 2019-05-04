a6c682d27ce2481f66c897f9ab4efb9a88856060
Yincheng Ren <yr5ka@virginia.edu>
2019-05-04 16:55:16 -0400

    dependency management

--- src/main/java/com/in28Minutes/spring/basics/springin10steps/BubbleSortAlgorithm.java
+++ src/main/java/com/in28Minutes/spring/basics/springin10steps/BubbleSortAlgorithm.java
@@ -1,5 +1,7 @@
 package com.in28Minutes.spring.basics.springin10steps;
+import org.springframework.stereotype.Component;
 
+@Component
 public class BubbleSortAlgorithm implements SortAlgorithm{
 	public int[] sort(int[] numbers) {
 		//logic of bubble sort
--- src/main/java/com/in28Minutes/spring/basics/springin10steps/SpringIn10StepsApplication.java
+++ src/main/java/com/in28Minutes/spring/basics/springin10steps/SpringIn10StepsApplication.java
@@ -2,15 +2,21 @@ package com.in28Minutes.spring.basics.springin10steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringIn10StepsApplication {
-
+	//beans
+	//dependency for beans
+	//where to find beans 
 	public static void main(String[] args) {
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+		//BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+		//Application Context
+		ApplicationContext applicationContext = SpringApplication.run(SpringIn10StepsApplication.class, args);
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
 		int result = binarySearch.binarySearch(new int [] {12,4,6}, 3);
 		System.out.println(result);
-		//SpringApplication.run(SpringIn10StepsApplication.class, args);
+
 	}
 
 }
